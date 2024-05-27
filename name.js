6c84e7fb7d116314a6e21b2ed6356e1faa4d611c
ReinerLau <lk850593913@gmail.com>
2024-05-27 10:27:28 +0800

    feat: vitest

--- name.js
+++
@@ -1,8 +0,0 @@
-const { UserStore } = require("./UserStore");
-
-function getUserDisplayName() {
-  const user = UserStore.getUser();
-  return `${user.LastName}, ${user.FirstName}`;
-}
-
-module.exports = getUserDisplayName;
