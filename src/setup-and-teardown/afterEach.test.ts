d7fa04d1e45c8f94af13252d0a3bf8f3d7f215e2
ReinerLau <lk850593913@gmail.com>
2025-07-15 08:48:11 +0800

    refactor: organize afterEach tests within a describe block for improved structure

--- src/setup-and-teardown/afterEach.test.ts
+++ src/setup-and-teardown/afterEach.test.ts
@@ -1,15 +1,17 @@
-import { afterEach, expect, test } from "vitest";
+import { afterEach, describe, expect, test } from "vitest";
 
-afterEach(() => {
-  console.log("每次测试后运行");
-});
+describe("每次测试后运行", () => {
+  afterEach(() => {
+    console.log("每次测试后运行");
+  });
 
-test("测试-1", () => {
-  console.log("测试-1");
-  expect(1).toBe(1);
-});
+  test("测试-1", () => {
+    console.log("测试-1");
+    expect(1).toBe(1);
+  });
 
-test("测试-2", () => {
-  console.log("测试-2");
-  expect(1).toBe(1);
+  test("测试-2", () => {
+    console.log("测试-2");
+    expect(1).toBe(1);
+  });
 });
