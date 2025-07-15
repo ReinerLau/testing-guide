121eaa85dc0a648b91c770aa68b0c04f0ee2151a
ReinerLau <lk850593913@gmail.com>
2025-07-15 10:22:16 +0800

    fix: update test description to clarify usage of __mocks__ folder implementation for the sum module

--- src/vi-utility/mock/mock-with-mocks-folder/mock.test.ts
+++ src/vi-utility/mock/mock-with-mocks-folder/mock.test.ts
@@ -5,7 +5,7 @@ import { sum } from "./module-1";
 // 使用 __mocks__ 文件夹中同名文件的实现
 vi.mock("./module-1");
 
-test("mock 模块并提供工厂函数", () => {
+test("mock 模块并使用 __mocks__ 文件夹中同名文件的实现", () => {
   plusOne(1, 2);
   expect(sum).toHaveBeenCalledWith(1, 2);
   expect(sum(2, 2)).toBe(3);
