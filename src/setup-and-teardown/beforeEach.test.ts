65f96c0e28e6f8c8855fcb3a620e7138837741ab
ReinerLau <lk850593913@gmail.com>
2025-07-15 08:44:57 +0800

    feat: add beforeEach setup and example tests in beforeEach.test.ts

---
+++ src/setup-and-teardown/beforeEach.test.ts
@@ -0,0 +1,15 @@
+import { beforeEach, expect, test } from "vitest";
+
+beforeEach(() => {
+  console.log("每次测试前运行");
+});
+
+test("测试-1", () => {
+  console.log("测试-1");
+  expect(1).toBe(1);
+});
+
+test("测试-2", () => {
+  console.log("测试-2");
+  expect(1).toBe(1);
+});
