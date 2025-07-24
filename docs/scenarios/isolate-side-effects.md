f80240645322d8e50623ec46e3c32fe206c0621f
ReinerLau <lk850593913@gmail.com>
2025-07-24 13:56:12 +0800

    refactor: update saveUserInfo and getUserInfo functions to accept parameters for better isolation of side effects

--- docs/scenarios/isolate-side-effects.md
+++ docs/scenarios/isolate-side-effects.md
@@ -10,40 +10,5 @@
 
 ## 示例
 
-对应 `src/scenarios/isolate-side-effects.test.ts`:
+[src/scenarios/isolate-side-effects.test.ts](https://github.com/ReinerLau/testing-guide/blob/main/src/scenarios/isolate-side-effects.test.ts)
 
-```ts
-/**
- * 汇总了副作用与核心的函数, 可以 Mock 后测试, 也可以不测试
- */
-function saveUserInfo() {
-  const user = getUserInfo();
-  saveToLocalStorage("userInfo", JSON.stringify(user));
-}
-
-/**
- * 需要测试的核心逻辑
- */
-function getUserInfo() {
-  return {
-    name: "John",
-    age: 20,
-  };
-}
-
-/**
- * 不需要测试的纯副作用逻辑
- */
-function saveToLocalStorage(key: string, value: string) {
-  localStorage.setItem(key, value);
-}
-
-describe("存储用户信息", () => {
-  it("获取用户信息", () => {
-    expect(getUserInfo()).toEqual({
-      name: "John",
-      age: 20,
-    });
-  });
-});
-```
--- src/scenarios/isolate-side-effects.test.ts
+++ src/scenarios/isolate-side-effects.test.ts
@@ -1,20 +1,21 @@
 import { describe, expect, it } from "vitest";
 
 /**
- * 汇总了副作用与核心的函数, 可以 Mock 后测试, 也可以不测试
+ * 汇总了副作用与核心的函数
+ * 可以 Mock saveToLocalStorage 后测试, 也可以不测试
  */
-function saveUserInfo() {
-  const user = getUserInfo();
+function saveUserInfo(name: string, age: number) {
+  const user = getUserInfo(name, age);
   saveToLocalStorage("userInfo", JSON.stringify(user));
 }
 
 /**
  * 需要测试的核心逻辑
  */
-function getUserInfo() {
+function getUserInfo(name: string, age: number) {
   return {
-    name: "John",
-    age: 20,
+    name,
+    age,
   };
 }
 
@@ -27,7 +28,7 @@ function saveToLocalStorage(key: string, value: string) {
 
 describe("存储用户信息", () => {
   it("获取用户信息", () => {
-    expect(getUserInfo()).toEqual({
+    expect(getUserInfo("John", 20)).toEqual({
       name: "John",
       age: 20,
     });
